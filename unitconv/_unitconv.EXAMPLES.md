8740e45227604bc8c0e31e7ebea1b98c7573e023
Nick Wells <wells.nicholas@gmail.com>
2025-03-07 21:47:42 +0000

    doc: Add examples showing the use of the 'roughly' parameter

--- unitconv/_unitconv.EXAMPLES.md
+++ unitconv/_unitconv.EXAMPLES.md
@@ -28,3 +28,9 @@ unitconv -from chain -to m -val 80 -just-val
 This will show 80 chains in metres\. Only the value is shown and no surrounding
 explanatory text
 
+```sh
+unitconv -from chain -to m -val 80 -roughly
+```
+This will show 80 chains in metres\. The value is adjusted to show the nearest
+multiple of 5 or 10
+
--- unitconv/addExamples.go
+++ unitconv/addExamples.go
@@ -15,5 +15,8 @@ func addExamples(ps *param.PSet) error {
 	ps.AddExample("unitconv -from chain -to m -val 80 -just-val",
 		"This will show 80 chains in metres. Only the value is "+
 			"shown and no surrounding explanatory text")
+	ps.AddExample("unitconv -from chain -to m -val 80 -roughly",
+		"This will show 80 chains in metres. The value is "+
+			"adjusted to show the nearest multiple of 5 or 10")
 	return nil
 }
