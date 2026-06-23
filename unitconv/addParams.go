1fdb4a510f1d5336494a148df8b3295652eb5e29
Nick Wells <wells.nicholas@gmail.com>
2026-06-23 12:36:24 +0100

    feature(unitconv): add the 'nearest' parameter family
    Also:
    - supply const values for the parameter names
    
    - add SeeAlso values to several parameters
    
    - expanded the documentation for some of the parameters
    
    - the parameters are checked to make sure that either 'nearest' or 'to' is
      chosen.
    
    - various alternative names have been added

--- unitconv/addParams.go
+++ unitconv/addParams.go
@@ -13,10 +13,22 @@ import (
 )
 
 const (
-	paramNameFrom        = "from"
-	paramNameTo          = "to"
+	paramNameFrom = "from"
+	paramNameTo   = "to"
+
+	paramNameNearest          = "nearest"
+	paramNameNearestCount     = "nearest-count"
+	paramNameNearestPrecision = "nearest-precision"
+	paramNameNearestIgnoreTag = "nearest-ignore-tag"
+
 	paramNameRoughly     = "roughly"
 	paramNameVeryRoughly = "very-roughly"
+
+	paramNameFamily    = "family"
+	paramNameValue     = "value"
+	paramNameJustValue = "just-value"
+	paramNameWidth     = "width"
+	paramNamePrecision = "precision"
 )
 
 const (
@@ -27,55 +39,140 @@ const (
 // addParams will add parameters to the passed ParamSet
 func addParams(prog *prog) func(ps *param.PSet) error {
 	return func(ps *param.PSet) error {
-		ps.Add(paramNameFrom, psetter.String[string]{Value: &prog.unitFromName},
+		const familyChoice = "The first family that has both" +
+			" the 'from' and 'to'" +
+			" units will be used." +
+			" You can force the family to use" +
+			" with the '" + paramNameFamily + "' parameter."
+
+		var toOrBestCounter paction.Counter
+
+		tOBCAF := toOrBestCounter.MakeActionFunc()
+
+		ps.Add(paramNameFrom,
+			psetter.String[string]{Value: &prog.unitFromName},
 			"The units the value is in."+
 				" It must be in the same family of units"+
-				" as the '"+paramNameTo+"' units.",
+				" as the '"+paramNameTo+"' units."+
+				"\n\n"+
+				familyChoice,
 			param.Attrs(param.MustBeSet),
+			param.SeeAlso(paramNameFamily, paramNameTo, paramNameNearest),
 		)
-		ps.Add(paramNameTo, psetter.StrList[string]{
-			Value: &prog.unitToNames,
-			Checks: []check.ValCk[[]string]{
-				check.SliceLength[[]string](check.ValGT(0)),
-				check.SliceAll[[]string](
-					check.StringLength[string](check.ValGT(0))),
+
+		ps.Add(paramNameNearest,
+			psetter.Bool{Value: &prog.nearestVal},
+			"Convert the value into some unit in the same family of"+
+				" units such that the quantity in that unit is some small,"+
+				" preferably whole number value. A range of alternatives will"+
+				" be shown.",
+			param.PostAction(tOBCAF),
+			param.SeeAlso(
+				paramNameTo,
+				paramNameNearestCount,
+				paramNameNearestPrecision,
+				paramNameNearestIgnoreTag,
+			),
+		)
+
+		nearestCountParam := ps.Add(paramNameNearestCount,
+			psetter.Int[int]{
+				Value: &prog.nearestCount,
+				Checks: []check.ValCk[int]{
+					check.ValGE(1),
+				},
 			},
-		},
+			"how many 'nearest' values should be shown.",
+			param.SeeAlso(
+				paramNameNearest,
+				paramNameNearestPrecision,
+				paramNameNearestIgnoreTag,
+			),
+		)
+
+		nearestPrecisionParam := ps.Add(paramNameNearestPrecision,
+			psetter.Float[float64]{
+				Value: &prog.nearestPrecision,
+				Checks: []check.ValCk[float64]{
+					check.ValGT(0.0),
+				},
+			},
+			"when generating the 'nearest' value,"+
+				" how close to a whole number value to we allow"+
+				" when comparing converted values.",
+			param.SeeAlso(
+				paramNameNearest,
+				paramNameNearestCount,
+				paramNameNearestIgnoreTag,
+			),
+		)
+
+		nearestIgnoreTagsParam := ps.Add(paramNameNearestIgnoreTag,
+			unitsetter.TagListAppender{
+				Value: &prog.nearestIgnoreTags,
+			},
+			"when generating the 'nearest' value,"+
+				" ignore any units with these tags.",
+			param.SeeAlso(
+				paramNameNearest,
+				paramNameNearestCount,
+				paramNameNearestPrecision,
+			),
+		)
+
+		ps.Add(paramNameTo,
+			psetter.StrList[string]{
+				Value: &prog.unitToNames,
+				Checks: []check.ValCk[[]string]{
+					check.SliceLength[[]string](check.ValGT(0)),
+					check.SliceAll[[]string](
+						check.StringLength[string](check.ValGT(0))),
+				},
+			},
 			"The units to convert the value into."+
 				" They must all be in the same family of units as"+
-				" the '"+paramNameFrom+"' unit.",
-			param.Attrs(param.MustBeSet),
+				" the '"+paramNameFrom+"' unit."+
+				"\n\n"+
+				familyChoice,
+			param.PostAction(tOBCAF),
+			param.SeeAlso(paramNameFamily, paramNameFrom, paramNameNearest),
 		)
-		ps.Add("family",
+
+		ps.Add(paramNameFamily,
 			unitsetter.FamilySetter{
 				Value: &prog.unitFamily,
 			},
-			"the family of units to use",
+			"the family of units to use."+
+				" The 'to' and 'from' units will be selected from this family.",
+			param.AltNames("f", "fam"),
+			param.SeeAlso(paramNameTo, paramNameFrom),
 		)
 
-		ps.Add("val", psetter.Float[float64]{Value: &prog.val},
+		ps.Add(paramNameValue, psetter.Float[float64]{Value: &prog.val},
 			"the value to be converted.",
-			param.AltNames("v"),
+			param.AltNames("v", "val"),
 		)
 
-		ps.Add("width", psetter.Int[int]{Value: &prog.displayWidth},
+		ps.Add(paramNameWidth, psetter.Int[int]{Value: &prog.displayWidth},
 			"the space to allow for the display of the"+
 				" converted value (the number part).",
+			param.SeeAlso(paramNamePrecision),
 		)
 
-		ps.Add("precision", psetter.Int[int]{Value: &prog.displayPrec},
+		ps.Add(paramNamePrecision, psetter.Int[int]{Value: &prog.displayPrec},
 			"the number of digits of precision to allow"+
 				" when displaying the"+
 				" converted value (the number part).",
 			param.AltNames("prec"),
+			param.SeeAlso(paramNameWidth),
 		)
 
-		ps.Add("just-val", psetter.Bool{Value: &prog.justVal},
+		ps.Add(paramNameJustValue, psetter.Bool{Value: &prog.justVal},
 			"just show the result of the conversion and not"+
 				" the from and to units as well."+
 				" This flag will make the result easier to use in"+
 				" scripts as only the result is shown.",
-			param.AltNames("short", "s"),
+			param.AltNames("just-val", "value-only", "val-only", "short", "s"),
 		)
 
 		ps.Add(paramNameRoughly, psetter.Nil{},
@@ -99,12 +196,35 @@ func addParams(prog *prog) func(ps *param.PSet) error {
 		)
 
 		ps.AddFinalCheck(func() error {
+			if toOrBestCounter.Count() != 1 {
+				return fmt.Errorf(
+					"you must give either %q or %q (not both)",
+					paramNameTo, paramNameNearest)
+			}
+
+			if prog.nearestVal {
+				return prog.findNearestVals()
+			}
+
+			if !prog.nearestVal &&
+				(nearestCountParam.HasBeenSet() ||
+					nearestPrecisionParam.HasBeenSet() ||
+					nearestIgnoreTagsParam.HasBeenSet()) {
+				return fmt.Errorf(
+					"unless the %q parameter is given"+
+						" the %q, %q or %q parameters have no effect",
+					paramNameNearest,
+					paramNameNearestCount,
+					paramNameNearestPrecision,
+					paramNameNearestIgnoreTag)
+			}
+
 			if prog.unitFamily != nil {
 				return populateTargetUnitsFromFamily(prog)
 			}
 
-			for _, fName := range units.GetFamilyNames() {
-				prog.unitFamily = units.GetFamilyOrPanic(fName)
+			for _, f := range units.GetFamilies() {
+				prog.unitFamily = f
 				if err := populateTargetUnitsFromFamily(prog); err == nil {
 					return nil
 				}
@@ -112,7 +232,7 @@ func addParams(prog *prog) func(ps *param.PSet) error {
 
 			return fmt.Errorf("there is no unit-family having both %q and %s",
 				prog.unitFromName,
-				english.JoinQuoted(prog.unitToNames, ", ", " and ", `"`, `"`))
+				english.JoinQuoted(prog.unitToNames, ", ", " and "))
 		})
 
 		return nil
